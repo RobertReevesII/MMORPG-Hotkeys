72b616fbcd2f3f1853feaf3222c9ab05e81398e8
Robert Reeves II <robertreevesii@roberts-mbp-2.netgear.com>
2015-12-16 21:30:27 -0800

    Initial Commit. Basic UI complete.

---
+++ VideoGameHotkeys/RLRHotkeysTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  RLRHotkeysTableViewController.h
+//  VideoGameHotkeys
+//
+//  Created by Robert Reeves II on 12/16/15.
+//  Copyright © 2015 Robert Reeves II. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RLRHotkeysTableViewController : UITableViewController
+
+@end
